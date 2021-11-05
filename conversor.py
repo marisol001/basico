e3d8d7bc15ef335dd9174ace6c5b16f90d058d24
marisol001 <reyes-espino-marisol@hotmail.com>
2021-11-04 23:16:13 -0600

    Agregando funciones al conversor de monedas

--- conversor.py
+++ conversor.py
@@ -6,32 +6,21 @@ menu = """
 
     Elige una opción:
 """
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("¿Cuántos pesos" + tipo_pesos + "tienes?: ")
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $ " + dolares + " dólares")
 
 opcion = int(input(menu))
 
 if opcion == 1:
-    pesos = input("¿Cuántos pesos colombianos tienes?:")
-    pesos = float(pesos)
-    valor_dolar = 3875
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $ " + dolares + " dólares")
+    conversor("colombianos", 3875)
 elif opcion == 2:
-    pesos = input("¿Cuántos pesos argetinos tienes?:")
-    pesos = float(pesos)
-    valor_dolar = 65
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $ " + dolares + " dólares")
+    conversor("argentinos", 65)
 elif opcion == 3:
-    pesos = input("¿Cuántos pesos mexicanos tienes?:")
-    pesos = float(pesos)
-    valor_dolar = 24
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $ " + dolares + " dólares")
+    conversor("mexicanos", 24)
 else:
     print("Ingresa una opción correcta, por favor")
